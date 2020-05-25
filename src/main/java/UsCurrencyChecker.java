8e28c922f741b557fcc67eb5918287c1cf8b04c9
alexanderHandstanger <zombeyfan.ah@gmail.com>
2020-05-25 09:27:36 +0200

    Second-Commit - 1 Test fail

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString)
     {
-        String regex = "\\$\\s*\\d+(.\\d+)?";
+        String regex = "\\$\\s*\\d+(.\\d{1,2})?";
 
         Pattern pattern = Pattern.compile(regex);
 
